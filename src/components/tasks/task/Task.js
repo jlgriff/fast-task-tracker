e2521e066917ca3a44ffee12d3052342b22b6b17
jlgriff <jlgriffith@protonmail.com>
2022-02-17 18:07:56 -0500

    Added css customization so that completed tasks are grayed out and cannot have their completed icon clicked anymore. Made other minor css tweaks.

--- src/components/tasks/task/Task.js
+++ src/components/tasks/task/Task.js
@@ -1,21 +1,32 @@
 import React from 'react';
-import CheckmarkIcon from '../../ui/icon/checkmark.ico'
-import TrashcanIcon from '../../ui/icon/trashcan.ico'
+import GreenCheckIcon from '../../ui/icon/green-check.ico'
+import GrayCheckIcon from '../../ui/icon/gray-check.ico'
+import RedTrashIcon from '../../ui/icon/red-trashcan.ico'
 
 import styles from './Task.module.css';
 import Card from "../../ui/card/Card";
 import TaskText from "./TaskText";
 
 const Task = props => {
+
+    const completed = props.completed;
+
     return (
         <li>
             <Card>
-                <div className={styles.task}>
-                    <img className={styles.icon} src={CheckmarkIcon} alt="checkmark icon"
-                         onClick={() => props.onComplete(props.id)}/>
+                <div className={`${styles.task} ${completed ? styles.completed : ''}`}>
+                    {completed
+                        ? <img src={GrayCheckIcon}
+                               alt="grayed-out checkmark icon"/>
+                        : <img className={styles.activeIcon}
+                               src={GreenCheckIcon}
+                               alt="checkmark icon"
+                               onClick={() => props.onComplete(props.id)}/>}
                     <TaskText title={props.title} description={props.description}/>
                     <div className={`${styles.push} ${styles.priority}`}>{props.priority}</div>
-                    <img className={styles.icon} src={TrashcanIcon} alt="trashcan icon"
+                    <img className={styles.activeIcon}
+                         src={RedTrashIcon}
+                         alt="trashcan icon"
                          onClick={() => props.onDelete(props.id)}/>
                 </div>
             </Card>
